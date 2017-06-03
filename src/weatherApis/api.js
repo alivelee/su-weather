0fe06b6de130f5f02cbcd30f9f35afe91a1b6c2e
Xu Lei <a871460434@gmail.com>
2017-06-04 00:07:24 +0800

    remove apis

--- src/weatherApis/api.js
+++ src/weatherApis/api.js
@@ -1,7 +1,5 @@
-export const DARKSKYKEY = "7d9ad9898f1ac7024b40e8b42e3102ae";
 export const APIXU = "58f51b418a7149d38c014454172405";
 export const OPENWEATHER = "&APPID=7531e3053ae0ef4d7851ab7b1438bae7";
 export const OPENWEATHERURL = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/";
-export const DARKSKYAPI = "https://api.darksky.net/forecast/";
 export const BAIDUURL = "https://api.map.baidu.com/api?v=2.0&ak=";
 export const BAIDUKEY = "L07FLumwGfWVL2kuqydtXHsp88RcFMSU";
